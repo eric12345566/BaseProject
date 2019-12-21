0e2a5aac0f25145a045d61ba9e84249a79bd0487
Steven <qazzaqqaz71@mail.fcu.edu.tw>
2019-12-21 16:25:04 +0800

    y

--- src/flutter_printpage/ios/Flutter/flutter_export_environment.sh
+++ src/flutter_printpage/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_v1.9.1+hotfix.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\flutterapp\flutter_printpage"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/qazzaqqaz71/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/qazzaqqaz71/BaseProject/src/flutter_printpage"
+export "FLUTTER_TARGET=/Users/qazzaqqaz71/BaseProject/src/flutter_printpage/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter_windows_v1.9.1+hotfix.6-stable\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/qazzaqqaz71/Downloads/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"

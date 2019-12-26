303ca109764d7ae3e94faba7f8da2b7847103479
Steven <qazzaqqaz71@mail.fcu.edu.tw>
2019-12-26 14:16:38 +0800

    yeh

--- src/flutter_app_test/lib/Home_Page.dart
+++ src/flutter_app_test/lib/Home_Page.dart
@@ -31,25 +31,28 @@ Future<void> showAlert(BuildContext context) {
   );
 }
 
+
+
+
 class Home_Page extends StatelessWidget {
   static const String id = "Home_Page";
 
   Future<List<User>> _getUsers() async {
-    var data = await http.get("");
+    var data = await http.get("http://ericlion.tw:3000/api/resturant/list/10");
 
     var jsonData = json.decode(data.body);
 
     List<User> users = [];
 
     for (var u in jsonData) {
-      User user = User(u["index"], u["name"], u["tag"]);
+      User user = User(u["rest_ID"], u["rest_name"], u["type"]);
 
       users.add(user);
 
       print(users.length);
 
-      return users;
     }
+    return users;
   }
 
 
@@ -59,72 +62,120 @@ class Home_Page extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: FutureBuilder(
-            future: _getUsers(),
-            builder: (BuildContext context, AsyncSnapshot snapshot){
-
-                if(snapshot.data == null){
-                  return Container(
-                    child: Center(
-                      child: Text("Loading..."),
+          child: SingleChildScrollView(
+          child: Column(
+          children: <Widget>[
+          SizedBox(
+          height: 50,
+        ),
+        Container(
+          child: Row(
+            children: <Widget>[
+              SizedBox(
+                width: 20,
+              ),
+              new RichText(
+                text: TextSpan(
+                    style: new TextStyle(
+                      fontSize: 23.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                    children: <TextSpan>[
+                      new TextSpan(text: '嗨! '),
+                      new TextSpan(
+                          text: Provider.of<Data>(context)
+                              .data['user']
+                              .toString(), // 使用者名稱取自 state
+                          style: new TextStyle(
+                              color: Colors.deepOrangeAccent)),
+                      new TextSpan(text: ' ，要'),
+                      new TextSpan(
+                          text: '吃晚餐',
+                          style: new TextStyle(
+                              color: Colors.deepOrangeAccent)),
+                      new TextSpan(text: '了嗎？'),
+                    ]),
+              ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      child: Image.asset(
+                        'image/韓國瑜.jpg',
+                        height: 70,
+                        width: 70,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Image.asset('image/IconSlot.png'),
+                      onPressed: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DinnerCard()));
+                      },
+                    ),
+                    FlatButton(
+                      child: Image.asset('image/IconCard.png'),
+                      onPressed: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Choose()));
+                      },
+                    ),
+                    FlatButton(
+                      child: Image.asset('image/IconMission.png'),
+                      onPressed: () {},
+                    ),
+                    FlatButton(
+                      child: Image.asset('image/IconWait.png'),
+                      onPressed: () {},
                     )
-                  );
-                }else {
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        width: 160.0,
-                        margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                        child: Card(
-                          child: InkWell(
-                            onTap: () async {
-                              {
-                                showAlert(context);
-                              }
-                            },
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset('image/coffee.png',
-                                    fit: BoxFit.fitWidth),
-                                SizedBox(height: 8.50),
-                                Container(
-                                    alignment: Alignment(-0.8, 10),
-                                    child: Text(
-                                    snapshot.data[index].name,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    )),
-                                SizedBox(height: 8.50),
-                                Container(
-                                    child: Text(
-                                      snapshot.data[index].tag,
-                                      style: TextStyle(fontSize: 10),
-                                    ))
-                              ],
-                            ),
-                          ),
-                          elevation: 5,
-                        ),
-                      );
-                    },
-                  );
-                }
-            },
+                  ],
+                ),
+              ),
+               SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                alignment: Alignment(-0.835, 0),
+                child: Text(
+                  '附近餐廳',
+                  style:
+                  TextStyle(fontWeight: FontWeight.bold, fontSize: 23.0),
+                ),
+              ),
+
+
+            ],
           ),
         ),
+        ],
       ),
-    );
-  }
+      ),
+    }
 }
 
 
 
 class User {
-  final int index;
-  final String name;
-  final String tag;
+  final int rest_ID;
+  final String rest_name;
+  final String type;
 
-  User(this.index, this.name, this.tag);
+  User(this.rest_ID, this.rest_name, this.type);
 }
